1d996464b4a2614c54f910b6d8d808be0bfa09c6
danushika1999 <39365226+danushika1999@users.noreply.github.com>
2018-10-02 10:01:20 +0200

    Update calcoli.js

--- calcoli.js
+++ calcoli.js
@@ -6,15 +6,15 @@ function calcolo(){
 	var sesso=document.forms.BMI.sesso.value;
 /*i dati non possono essere vuoti--> required */
 //verifico se l'utente è m o f
-	if (sesso='m'){
+	if(sesso='m'){
 	//applico la formula BMI per maschi
-	var totale= peso/(altezza*altezza);
+	var totale=peso/(altezza*altezza);
 /*Math.pow(base,exp)
 *Math.pow(altezza,2)*/
 document.getElementById('calcola').innerHTML="BMI : " + totale;
   
 	}
-	else if(sesso="f"){
+	else if(sesso='f'){
 //applico la formula BMI per femmine
  	var totale=(peso/(altezza*altezza))*0.9;
 	document.getElementById('calcola').innerHTML="BMI : " + totale;
