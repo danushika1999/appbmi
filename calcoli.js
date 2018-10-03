ca889db70ef8e890bee4db1b9ef8a062f7deb464
danushika1999 <39365226+danushika1999@users.noreply.github.com>
2018-10-03 14:03:37 +0200

    Update calcoli.js

--- calcoli.js
+++ calcoli.js
@@ -1,29 +1,62 @@
-/*clcoli per la app sul BMI*/
+/* calcoli in js */
 function calcolo(){
-//identificare i dati del form
-	var peso=document.forms.BMI.peso.valueAsNumber;
-	var altezza=document.forms.BMI.altezza.valueAsNumber;
-	var sesso=document.forms.BMI.sesso.value;
-/*i dati non possono essere vuoti--> required */
-//verifico se l'utente è m o f
-	if(sesso='m'){
-	//applico la formula BMI per maschi
-	var totale=peso/(altezza*altezza);
-/*Math.pow(base,exp)
-*Math.pow(altezza,2)*/
-document.getElementById('calcola').innerHTML="BMI : " + totale;
-  
+	/* dichiarazione variabili del form */
+    var peso=document.forms.BMI.peso.valueAsNumber;
+    var altezza=document.forms.BMI.altezza.valueAsNumber;
+    var sesso= document.forms.BMI.sesso.value;
+    /* i dati non possono essere vuoti --> required */
+    if(sesso = "m"){
+	var totale = peso/(altezza*altezza);
+	document.getElementById('calcola').innerHTML="BMI ="+totale;
+	if(totale<20.1){
+		document.getElementById('spiegazione').innerHTML="Sottopeso";
 	}
-	else if(sesso='f'){
-//applico la formula BMI per femmine
- 	var totale=(peso/(altezza*altezza))*0.9;
-	document.getElementById('calcola').innerHTML="BMI : " + totale;
-  
+	else if(totale<25){
+		document.getElementById('spiegazione').innerHTML="Peso Ottima";
 	}
+	else if(totale<29.9){
+		document.getElementById('spiegazione').innerHTML="Sovrappeso";
+	}
+	else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesità di 1°grado";
+	}
+	else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesità di 2°grado";
+	}
+	else if(totale>40){
+		document.getElementById('spiegazione').innerHTML="Obesità alto grado";
+	}
+	
+    else if(sesso="f"){
+	var totale = (peso/(altezza*altezza))*0.9;
+	document.getElementById('calcola').innerHTML="BMI ="+totale;
+	if(totale<18.7){
+		document.getElementById('spiegazione').innerHTML="Sottopeso";
+	}
+else if(totale<23.8){
+		document.getElementById('spiegazione').innerHTML="Peso Ottima";
+	}
+	else if(totale<28.6){
+		document.getElementById('spiegazione').innerHTML="Sovrappeso";
+	}
+	else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesità di 1°grado";
+	}
+	else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesità di 2°grado";
+	}
+	else if(totale>40){
+		document.getElementById('spiegazione').innerHTML="Obesità di alto grado";
+	}
+	
+	
+	}
+	
 }
 
   function cancella(){
-	document.getElementById('calcola').innerHTML=" ";  
-	  
+    document.getElementById('calcola').innerHTML=" "; 
   }
-
+function cancella(){
+    document.getElementById('spiegazione').innerHTML=" "; 
+  }
